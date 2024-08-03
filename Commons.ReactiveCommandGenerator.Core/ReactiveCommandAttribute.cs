5ac9fd1fdbfc2e94feef5c4b369b12ef634c6063
Avabin <andrzej.piotrowski76@gmail.com>
2024-08-03 18:26:31 +0200

    Add docs and nuget packages

--- Commons.ReactiveCommandGenerator.Core/ReactiveCommandAttribute.cs
+++ Commons.ReactiveCommandGenerator.Core/ReactiveCommandAttribute.cs
@@ -1,9 +1,19 @@
 ﻿namespace Commons.ReactiveCommandGenerator.Core;
 
+/// <summary>
+/// Attribute to mark a method to reactive command generation
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class ReactiveCommandAttribute : Attribute
 {
+    /// <summary>
+    /// Optional method name to check if the command can be executed
+    /// </summary>
     public string? CanExecuteMethodName { get; }
+    /// <summary>
+    /// Create a new instance of the attribute
+    /// </summary>
+    /// <param name="canExecuteMethodName">Optional method name to check if the command can be executed</param>
     public ReactiveCommandAttribute(string? canExecuteMethodName = null)
     {
         CanExecuteMethodName = canExecuteMethodName;
